3c9f1057e2d8f7b1122064d017918ffc4252d555
Hyder <77566984+hyder-cloud@users.noreply.github.com>
2024-06-18 17:22:45 +0300

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World!"
+    return "Hello World! changed by Hyder at 5:22 pm"
 
 if __name__ == "__main__":
     application.run()
